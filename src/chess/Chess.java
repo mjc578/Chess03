0c906caa6dea92a7b63ee35622b989d25a4f7a69
Michael Chapman <mjc578@gmail.com>
2018-10-16 23:36:44 -0400

    Committing before deadline

---
+++ src/chess/Chess.java
@@ -0,0 +1,11 @@
+package chess;
+
+public class Chess {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Wow chess project");
+		System.out.println("Wow overload");
+	}
+
+}
